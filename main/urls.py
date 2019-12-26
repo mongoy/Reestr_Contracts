e14e264e0eae192b8bf6c89b0f13db8686d356ae
mongoy <mongoy@rambler.ru>
2019-12-26 17:36:51 +0900

    edit urls, views, templates page contracts list

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RoadInfoView
+from .views import RoadInfoView, RoadListView
 
 urlpatterns = [
     path('', RoadInfoView.as_view(), name='index'),
+    path('list/', RoadListView.as_view(), name='contract-list'),
 ]
+
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.db.models import Sum, Count
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView
 from django.views.generic.base import View
 from .models import Reestr
 
@@ -14,7 +15,7 @@ class RoadInfoView(View):
     # def get(self, request, *args, **kwargs):
     @staticmethod
     def get(request):
-        info = Reestr.objects.all().aggregate(Count('num_contract', distinct=True), Sum('c_contract'))
+        info = Reestr.objects.all().aggregate(Count('id', distinct=True), Sum('c_contract'))
         qs = Reestr.objects.all().filter(work_contract=True)
         d_today = datetime.date.today()
         summ_ost = 0
@@ -23,4 +24,12 @@ class RoadInfoView(View):
         info['oroad__sum'] = summ_ost
         info['date__today'] = d_today
         return render(request, 'index.html', context=info)
+
+
+class RoadListView(ListView):
+    """Перечень дорог для просмотра"""
+    model = Reestr
+    queryset = Reestr.objects.all().filter(work_contract=True)
+    template_name = 'reestr/cotract_list.html'
+    paginate_by = 10
 # Create your views here.
